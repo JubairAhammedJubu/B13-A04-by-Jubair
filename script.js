1738daede08651201fd3a1d3d78cc2dee1f632d9
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-02-23 15:14:58 +0600

    js start and create button color and text change

---
+++ script.js
@@ -0,0 +1,58 @@
+let interviewList = [];
+let rejectedList = [];
+let currentStatus = "all";
+
+let total = document.getElementById("total");
+let interviewCount = document.getElementById("interviewCount");
+let rejectedCount = document.getElementById("rejectedCount"); 
+
+const allFilterBtn = document.getElementById("all-filter-btn");
+const interviewFilterBtn = document.getElementById("interview-filter-btn");
+const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+const allCardSection = document.getElementById("allCards");
+const mainContainer = document.querySelector("main");
+const filterSection = document.getElementById("filtered-section");
+
+function calculateCount() {
+  total.innerText = allCardSection.children.length;
+  interviewCount.innerText = interviewList.length;
+  rejectedCount.innerText = rejectedList.length;
+}
+
+calculateCount();
+
+
+function toggleStyle(id) {
+  // Reset all buttons
+  allFilterBtn.classList.add("bg-white", "text-black");
+  interviewFilterBtn.classList.add("bg-white", "text-black");
+  rejectedFilterBtn.classList.add("bg-white", "text-black");
+
+  allFilterBtn.classList.remove("bg-blue-500", "text-white");
+  interviewFilterBtn.classList.remove("bg-blue-500", "text-white");
+  rejectedFilterBtn.classList.remove("bg-blue-500", "text-white");
+
+  const selected = document.getElementById(id);
+  currentStatus = id;
+
+
+  selected.classList.remove("bg-white", "text-black");
+  selected.classList.add("bg-blue-500", "text-white");
+
+  
+  if (id === "interview-filter-btn") {
+    allCardSection.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+    renderInterview();
+  } else if (id === "all-filter-btn") {
+    allCardSection.classList.remove("hidden");
+    filterSection.classList.add("hidden");
+  } else if (id === "rejected-filter-btn") {
+    allCardSection.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+    renderRejected();
+  }
+}
+
+
